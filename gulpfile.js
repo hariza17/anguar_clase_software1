c7405a1d2176f6a9d357cb43ef271b8d6bed503c
Hernando.ariza <hernando.ariza@ormuco.com>
2017-04-25 18:24:44 -0500

    basic menu using angular-ui-router

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    del = require('del'),
+    browserify = require('browserify');
+
+
+var CacheBuster = require('gulp-cachebust');
+var cachebust = new CacheBuster();
+
+
+gulp.task('clean', function (cb) {
+    del(['dist'], cb);
+});
+
+gulp.task('bower', function () {
+
+    var install = require("gulp-install");
+
+    return gulp.src(['./bower.json'])
+        .pipe(install());
+});
+
+gulp.task('build', [ 'clean', 'bower'], function() {
+    return gulp.src('index.html')
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch(['./index.html', './partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
+});
+
+gulp.task('webserver', ['watch'], function () {
+
+
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: "http://localhost:7000/index.html"
+        }));
+});
+
+gulp.task('dev', ['clean','watch', 'webserver']);
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 var app = angular.module("app", ['ui.router']);
 
-app.config(['$stateProvider',function ($stateProvider) {
+app.config(function ($stateProvider,$urlRouterProvider) {
 
-        $stateProvider
-            .state('main', {
-                url: '/app',
-                templateUrl: 'js/views/main.html',
-            })
+    $urlRouterProvider.otherwise('/app');
+    $stateProvider
+        .state('main', {
+            url: '/app',
+            templateUrl: 'js/views/main.html'
 
-}]);
+        });
+
+    $stateProvider
+        .state('main.home', {
+            url: '/home',
+            templateUrl: 'js/views/home.html'
+
+        });
+    $stateProvider
+        .state('main.profile', {
+            url: '/profile',
+            templateUrl: 'js/views/profile.html'
+
+        });
+
+    $stateProvider
+        .state('main.messages', {
+            url: '/messages',
+            templateUrl: 'js/views/messages.html'
+
+        });
+
+});
 
 
 
